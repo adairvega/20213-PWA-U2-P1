15d06d884035634e5bbbb13404283c01420a2cbf
Adair Emanuel González Vega <51676730+adairvega@users.noreply.github.com>
2021-09-20 17:47:35 -0500

    [ADD] manifest file

--- js/app.js
+++ js/app.js
@@ -1,8 +1,16 @@
 console.log("Hola mundo desde app.js");
 
+const url = window.location.href;
+let swPath = '/20213-PWA-U2-P1/sw.js';
+
 if (navigator.serviceWorker) {
     console.log("Muy bien, podemos jugar :D");
-    navigator.serviceWorker.register('/sw.js');
+
+    if (url.includes('localhost')) {
+        swPath='/sw.js'
+    }
+
+    navigator.serviceWorker.register(swPath);
 } else {
     console.log("Lastima cambia de navegador");    
 }
